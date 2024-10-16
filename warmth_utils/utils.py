440efa1c3ff44af0c6a0586552864fe1a1cb9403
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2024-10-16 07:01:03 +0000

    get maxzoom

--- warmth_utils/utils.py
+++ warmth_utils/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from shapely import Point, Polygon
 
 from warmth_utils.spec import TectonicModel
@@ -16,3 +17,8 @@ def find_tectonic_model_index(point: list[float], tectonic: TectonicModel) -> in
             return idx
         all_distance.append(distance)
     return all_distance.index(min(all_distance))
+
+RESOLUTION_LOD0: float = 8000
+def get_max_zoom(xStep: float|int, yStep: float|int) -> int:
+    step = (xStep, yStep)
+    return int(np.fmax(0, int(np.log2(RESOLUTION_LOD0 / np.min(step)))) + 1)
