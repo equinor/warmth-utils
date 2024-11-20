0cd0940326676608e78d6abdd84ec7bb4f188d0f
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2024-11-20 09:34:13 +0000

    no auth

--- warmth_utils/auth.py
+++ warmth_utils/auth.py
@@ -1,12 +1,15 @@
 import msal
 from warmth_utils.config import config
 
-
-msal_client = msal.ConfidentialClientApplication(str(config.CLIENT_ID), config.CLIENT_SECRET,f'https://login.microsoftonline.com/{config.TENANT_ID}')
+if config.AUTH_MODE == "Azure":
+    msal_client = msal.ConfidentialClientApplication(str(config.CLIENT_ID), config.CLIENT_SECRET,f'https://login.microsoftonline.com/{config.TENANT_ID}')
+else:
+    msal_client = None
 
 def msal_token():
-    if config.AUTH_MODE == "Azure":
+    if isinstance(msal_client, type(None)):
+        return None
+    else:
         resp = msal_client.acquire_token_for_client([f'{config.OSDURESOURCEID}/.default'])
         return f"Bearer {resp['access_token']}"
-    else:
-        return None
+        
