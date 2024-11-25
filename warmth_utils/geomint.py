4fa7ba160e7f640340e4f73f99e5c16d099f8d06
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2024-11-25 13:34:44 +0000

    model xtgeo polygon obj

--- warmth_utils/geomint.py
+++ warmth_utils/geomint.py
@@ -1,3 +1,4 @@
+import xtgeo
 from warmth_utils.spec import GeomintFullModel
 from warmth_utils.config import MODEL_SPEC
 import json
@@ -6,3 +7,9 @@ import json
 with open(MODEL_SPEC, 'r') as f:
     model_spec_dict = json.load(f)
     model_spec = GeomintFullModel.parse_obj(model_spec_dict)
+    
+pts = []
+for p in model_spec.model.aoi:
+    p.extend([0, 1])
+    pts.append(p)
+model_xtgeo_polygon = xtgeo.Polygons(pts)
