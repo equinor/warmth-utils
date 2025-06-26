cee934cf5e94dd73a9985e7c02f2253642052ddc
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2025-06-26 07:53:30 +0000

    output age ranges

--- warmth_utils/geomint.py
+++ warmth_utils/geomint.py
@@ -14,3 +14,19 @@ for p in model_spec.model.aoi:
     p.extend([0, 1])
     pts.append(p)
 model_xtgeo_polygon = xtgeo.Polygons(pts)
+
+def age_ranges(youngest, oldest, interval):
+    r = list(range(youngest, oldest, interval))
+    r.append(oldest)
+    return r
+input_horizons_ages = [int(i.age) for i in model_spec.model.framework.geometries]
+rift_events = [i.riftEvents for i in model_spec.model.tectonicModel.domains]
+rift_ages = [age_ranges(j.end, j.start, 2)  for i in rift_events for j in i]
+
+output_ages = age_ranges(min(input_horizons_ages), max(input_horizons_ages), 5)
+
+for i in rift_ages:
+    output_ages.extend(i)
+
+output_ages = list(set(output_ages))
+output_ages.sort()
